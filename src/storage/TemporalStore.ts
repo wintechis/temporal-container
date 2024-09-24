7191b7f36a9d83edb222547d802896c6966ee6b7
Daniel Schraudner <daniel.schraudner@fau.de>
2024-09-24 15:26:07 +0200

    Temporal operators

--- src/storage/TemporalStore.ts
+++ src/storage/TemporalStore.ts
@@ -9,6 +9,7 @@ import { toCanonicalUriPath } from '../util/PathUtil';
 import { BasicRepresentation } from '../http/representation/BasicRepresentation';
 import { RepresentationMetadata } from '../http/representation/RepresentationMetadata';
 import { once } from 'events';
+import { parse, toSeconds } from 'iso8601-duration';
 
 /* eslint-disable unused-imports/no-unused-vars */
 export class TemporalStore<T extends ResourceStore = ResourceStore> extends PassthroughStore<T> {
@@ -21,6 +22,8 @@ export class TemporalStore<T extends ResourceStore = ResourceStore> extends Pass
     preferences: RepresentationPreferences,
     conditions?: Conditions
   ): Promise<Representation> { 
+    const now = Date.now();
+
     // Extract and remove search params from uri
     let uri = new URL(identifier.path);
     const searchParams = new URLSearchParams(uri.searchParams);
@@ -34,7 +37,7 @@ export class TemporalStore<T extends ResourceStore = ResourceStore> extends Pass
           DataFactory.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
           DataFactory.namedNode('https://solid.ti.rw.fau.de/public/ns/tc#TemporalContainer')
         )) {
-          let csv = '';
+          let values = [];
           let containedResources = rep.metadata.getAll(DataFactory.namedNode('http://www.w3.org/ns/ldp#contains'));
           for(let cR of containedResources) {
             let cRep = await this.source.getRepresentation({ path: cR.value }, preferences, conditions);
@@ -48,24 +51,92 @@ export class TemporalStore<T extends ResourceStore = ResourceStore> extends Pass
               null
             );
             for(let obs of observations) {
+              // filter observedProperty
+              if(searchParams.has('observedProperty') && !store.has(DataFactory.quad(
+                obs,
+                DataFactory.namedNode('http://www.w3.org/ns/sosa/observedProperty'),
+                DataFactory.namedNode(searchParams.get('observedProperty')!)
+              ))) {
+                continue;
+              }
+
+              // filter madeBySensor
+              if(searchParams.has('madeBySensor') && !store.has(DataFactory.quad(
+                obs,
+                DataFactory.namedNode('http://www.w3.org/ns/sosa/madeBySensor'),
+                DataFactory.namedNode(searchParams.get('madeBySensor')!)
+              ))) {
+                continue;
+              }
+
               let timestamps = store.getObjects(obs, DataFactory.namedNode('http://www.w3.org/ns/sosa/resultTime'), null).map(nn => nn.value);
               let results = store.getObjects(
                 obs,
                 DataFactory.namedNode('http://www.w3.org/ns/sosa/hasResult'),
                 null
               ).flatMap(
-                resultObject => store.getObjects(
-                  resultObject,
-                  DataFactory.namedNode('http://qudt.org/vocab/unit/numericValue'),
-                  null
-                ).map(nn => nn.value)
+                (resultObject) => {
+                  return [store.getObjects(
+                    resultObject,
+                    DataFactory.namedNode('http://qudt.org/vocab/unit/numericValue'),
+                    null
+                  ).map(nn => nn.value),
+                  store.getObjects(
+                    resultObject,
+                    DataFactory.namedNode('http://qudt.org/schema/qudt/hasUnit'),
+                    null
+                  ).map(nn => nn.value)];
+                }
               )
-              if(timestamps.length > 0 && results.length > 0) {
-                csv += `${timestamps[0]}, ${results[0]}\n`
+              if(timestamps.length > 0 && results.length > 0 && results[0].length > 0) {
+                values.push([timestamps[0], results[0][0], results[1][0]]);
               } 
             }
           }
-          resolve(new BasicRepresentation(csv, new RepresentationMetadata({ path: uri.href }, 'text/csv')));
+          // filter intervals
+          if(searchParams.has('intervalStart')) {
+            let start = new Date(now.valueOf() - toSeconds(parse(searchParams.get('intervalStart')!)) * 1000);
+            values = values.filter(([ts, va, un]) => ts <= start.toISOString());
+          }
+          if(searchParams.has('intervalEnd')) {
+            let end = new Date(now.valueOf() - toSeconds(parse(searchParams.get('intervalEnd')!)) * 1000);
+            values = values.filter(([ts, va, un]) => ts >= end.toISOString());
+          }
+          if(searchParams.has('intervalAbsoluteStart')) {
+            let start = new Date(searchParams.get('intervalAbsoluteStart')!);
+            values = values.filter(([ts, va, un]) => ts <= start.toISOString());
+          }
+          if(searchParams.has('intervalAbsoluteEnd')) {
+            let end = new Date(searchParams.get('intervalAbsoluteEnd')!);
+            values = values.filter(([ts, va, un]) => ts >= end.toISOString());
+          }
+
+          let orginalValueCount = values.length;
+
+          if(searchParams.has('value')) {
+            let value = searchParams.get('value')!;
+            if(value.startsWith('gte_')) {
+              values = values.filter(([ts, va, un]) => va >= value.replace('gte_', ''));
+            } else if(value.startsWith('gt_')) {
+              values = values.filter(([ts, va, un]) => va > value.replace('gt_', ''));
+            } else if(value.startsWith('lt_')) {
+              values = values.filter(([ts, va, un]) => va < value.replace('lt_', ''));
+            } else if(value.startsWith('lte_')) {
+              values = values.filter(([ts, va, un]) => va <= value.replace('lte_', ''));
+            } else {
+              values = values.filter(([ts, va, un]) => va == value);
+            }
+          }
+
+          // temporal operators
+          if(searchParams.get('operator') == 'diamond') {
+            resolve(new BasicRepresentation(`${values.length > 0}`, new RepresentationMetadata({ path: uri.href }, 'application/json')));
+          } else if(searchParams.get('operator') == 'box') {
+            resolve(new BasicRepresentation(`${values.length == orginalValueCount}`, new RepresentationMetadata({ path: uri.href }, 'application/json')));
+          } else {
+            let csv = values.sort().map(v => `${v[0]}, ${v[1]}, ${v[2]}`).join('\n');
+            resolve(new BasicRepresentation(csv, new RepresentationMetadata({ path: uri.href }, 'text/csv')));
+          }
         }
       });
     } else {
